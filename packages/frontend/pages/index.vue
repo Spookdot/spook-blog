1b1ce3e583407a19d921603382f97ae64330bd44
Spookdot <333.333.333@gmx.de>
2022-04-30 00:41:39 +0200

    Make Posts available via public role

--- packages/frontend/pages/index.vue
+++ packages/frontend/pages/index.vue
@@ -11,11 +11,16 @@
 import { Post } from "~/types";
 import { Strapi4Response } from "@nuxtjs/strapi";
 
+definePageMeta({
+  middleware: ["strapi-auth"],
+});
+
 const client = useStrapiClient();
+const token = useStrapiToken();
 
 const posts = await client<Strapi4Response<Post>>("posts", {
   headers: {
-    Authorization: "Bearer " + process.env.API_TOKEN,
+    Authorization: "Bearer " + token,
   },
 });
 </script>
